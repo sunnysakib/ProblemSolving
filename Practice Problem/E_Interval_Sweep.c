04017ab812a0d4dd555c4bdf6fe336e4e6adbd97
sunnySakib <sakibur.rahaman033@gmail.com>
2025-06-13 10:24:28 +0600

    Add E_Interval_Sweep.c for interval sweep problem

---
+++ Practice Problem/E_Interval_Sweep.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    scanf("%d%d", &a,&b);
+
+    if(a == 0 && b == 0) printf("NO\n");
+    else if(a==b || a-b==1 || b-a==1){
+        printf("YES\n");
+    }else{
+        printf("NO\n");
+    }
+    
+    return 0;
+}
