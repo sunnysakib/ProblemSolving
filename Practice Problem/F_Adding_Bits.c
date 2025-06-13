e28a31c33fcc341d5569920f08f1baee46fafc9e
sunnySakib <sakibur.rahaman033@gmail.com>
2025-06-13 10:42:41 +0600

    Add F_Adding_Bits.c for bitwise addition problem

---
+++ Practice Problem/F_Adding_Bits.c
@@ -0,0 +1,13 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/F
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    scanf("%d%d", &a,&b);
+
+    printf("%d\n", a^b);
+    
+    return 0;
+}
