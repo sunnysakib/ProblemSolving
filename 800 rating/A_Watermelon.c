e5b9a2bc9c0e3b01327aef77a8846de7054ae2b4
sunnySakib <sakibur.rahaman033@gmail.com>
2025-06-16 06:25:42 +0600

    Add A_Watermelon.c for watermelon problem solution

---
+++ 800 rating/A_Watermelon.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+typedef long long ll;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    if(t==2) printf("NO\n");
+    else if(t%2==0) printf("YES\n");
+    else printf("NO\n");
+    
+
+    return 0;
+}
